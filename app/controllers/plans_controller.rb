ba72a140b866ae87ae95a3326dc38590d21c601b
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-11 18:36:39 +0900

    Add Planのnewアクション関連を作成

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,23 @@
 class PlansController < ApplicationController
+  def new
+    @plan = Plan.new
+  end
+  def create
+    @plan = current_user.plans.new(plan_params)
+    if @plan.save
+      redirect_to plans_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @plan = Plan.all
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:name, :content)
+  end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,6 @@
 class Plan < ApplicationRecord
+  belongs_to :user
+  
+  validates :name, presence: true
+
 end
