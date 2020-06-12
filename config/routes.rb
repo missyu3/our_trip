ba7fff1d96a8eadc0d77e2e9619ba0f62040b29c
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-12 17:24:14 +0900

    Add rails g device　でViewとControllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root 'plans#index'
   resources :plans
-  devise_for :users
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+  } 
+
+devise_scope :user do
+  get "user/:id", :to => "users/registrations#detail"
+  get "signup", :to => "users/registrations#new"
+  get "login", :to => "users/sessions#new"
+  get "logout", :to => "users/sessions#destroy"
+end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
