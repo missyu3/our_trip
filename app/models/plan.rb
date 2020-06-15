269646e2aeebbc8fe89cdd155e08e0beb7cc8944
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-15 23:27:39 +0900

    Add plan_itemにバリデーションを追加

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,7 +1,7 @@
 class Plan < ApplicationRecord
   belongs_to :user
   has_many :plan_items, dependent: :destroy
-  
+
   validates :name, presence: true
 
   scope :order_by_updated_before, -> { order(updated_at: :desc) }
--- app/models/plan_item.rb
+++ app/models/plan_item.rb
@@ -4,6 +4,9 @@ class PlanItem < ApplicationRecord
 
   enum categories: { nothing: 0, move: 1, activity: 2, meal: 3, lodging: 4 }
 
+  validates :title   , presence: true
+  validates :content , presence: true
+
   scope :order_by_updated_before, -> { order(updated_at: :desc) }
 
 end
