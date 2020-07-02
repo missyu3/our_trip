2c00bea359070daf07943e7d5696c5c052432508
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-07-02 15:41:16 +0900

    Add FactoryBotで画像データを登録するように追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,6 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
       "uploads_#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     else
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :plan do
     name { '北海道' }
     content { '海産物が美味しいよ！' }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'factories', 'test.jpg')) }
     user_id { 1 }
   end
 end
