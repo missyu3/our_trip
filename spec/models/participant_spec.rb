ca9056580abfbd2a1225a99d5362dae90a2aa255
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-07-02 10:37:41 +0900

    Add participantのModelSpecを追加

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -3,5 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Participant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_test) { create(:user) }
+  let(:plan) { create(:plan, user_id: user_test.id) }
+  it 'データを削除できるか' do
+    participant1 = create(:participant, plan_id: plan.id , user_id: user_test.id)
+    expect{ participant1.destroy }.to change{ Participant.count }.by(-1)
+  end
+  it '登録処理が成功した時、"ユーザーを追加しました。"」と返ってくるか' do
+    participant1 = Participant.check_and_create(plan.id,user_test.name)
+    expect(participant1).to eq "ユーザーを追加しました。"
+  end
+  it '登録されてないユーザーを参加させた時、「「存在しないユーザーです」が返ってくるか' do
+    participant1 = Participant.check_and_create(plan.id,"hogehoge")
+    expect(participant1).to eq "存在しないユーザーです"
+  end
+  it 'すでに参加しているユーザーを参加させようとした時、「そのユーザーはすでに参加済みです」が返ってくるか' do
+    participant1 = Participant.check_and_create(plan.id,user_test.name)
+    participant2 = Participant.check_and_create(plan.id,user_test.name)
+    expect(participant2).to eq "そのユーザーはすでに参加済みです"
+  end
+
 end
