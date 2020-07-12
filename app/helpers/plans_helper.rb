35a6ed854bff37c414fa086eedddf1ba2d1f9884
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-07-12 16:47:16 +0900

    Fix レイアウトを修正

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module PlansHelper
+  def chose_plan_image(plan)
+    if plan.image.present?
+      user.image.url
+    else
+      'travel.jpg'
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module UsersHelper
+
+  def chose_user_icon(user)
+    if user.image.present?
+      user.image.url
+    else
+      "human.png"
+    end
+  end
 end
