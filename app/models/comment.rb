e378d7e3fd4a3cd43e2c2c13ee441de664447354
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-07-16 18:45:44 +0900

    Fix 無限スクロールが正常に動かなかったため、削除

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,6 +8,6 @@ class Comment < ApplicationRecord
   scope :order_by_id_desc, -> { order(id: "DESC") }
 
   def self.get_comments_by_kaminari(plan_item_id,params_page)
-    self.plan_item_relation(plan_item_id).page(params_page).per(10).order_by_id_desc
+    self.plan_item_relation(plan_item_id).includes(:user).order_by_id_desc.page(params_page).per(10)
   end
 end
