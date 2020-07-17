4f9abb72be144e4f73e734b53da204574b00191b
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-07-17 14:42:46 +0900

    Ref I18n対応と不要なグローバル変数をローカル変数に変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new
-    @plan_item = PlanItem.find_by(id: params[:plan_item_id])
-    @comment =  Comment.new(comment_params)
-    @comment.plan_item_id = @plan_item.id
-    @comment.user_id = current_user.id
+    comment = Comment.new
+    plan_item = PlanItem.find_by(id: params[:plan_item_id])
+    comment =  Comment.new(comment_params)
+    comment.plan_item_id = plan_item.id
+    comment.user_id = current_user.id
     respond_to do |format|
-      @comment.save
-      @comments = Comment.get_comments_by_kaminari(@plan_item.id,params[:page])
+      comment.save
+      @comments = Comment.get_comments_by_kaminari(plan_item.id,params[:page])
       format.js { render :index }
     end
   end
