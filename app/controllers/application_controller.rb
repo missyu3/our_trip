e4ed3f81c7eb43fcd79f340f00de079ab596dd4b
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-07-20 00:34:40 +0900

    Fix ページネーションの設定を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  KAMINARI_PER = 5
+  KAMINARI_PER = 10
 
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
