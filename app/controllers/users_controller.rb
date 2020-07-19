60d9a552627e0c63c2ac925ce7e2f51dadc96d94
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-07-19 23:44:30 +0900

    Add userにページネーションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
+    @plans = Plan.where(user_id: @user.id).includes(:user).page(params[:page]).per(KAMINARI_PER)
   end
 end
