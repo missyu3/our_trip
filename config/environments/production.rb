7adcf7033742cee15dac787fdc790016afa1c736
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-08-07 16:27:57 +0900

    Add aws-sdk-rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  creds = Aws::Credentials.new(
+    ENV['AWS_ACCESS_KEY_ID'],
+    ENV['AWS_SECRET_ACCESS_KEY']
+  )
+  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: creds, region: 'us-east-1')
+  config.action_mailer.delivery_method  =  :aws_sdk
 end
