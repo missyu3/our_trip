c0e1bb98b811c93a1c40acbc59162d56b7ad0553
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-09-17 23:43:20 +0900

    Add spot column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_045324) do
+ActiveRecord::Schema.define(version: 2020_09_17_141648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 2020_09_13_045324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "budget"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["plan_id"], name: "index_plan_items_on_plan_id"
     t.index ["user_id"], name: "index_plan_items_on_user_id"
   end
