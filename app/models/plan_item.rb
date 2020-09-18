9509c64845a326ec7dd08c42a98152e3f4bcd46a
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-09-18 15:51:38 +0900

    Add geocoder background

--- app/models/plan_item.rb
+++ app/models/plan_item.rb
@@ -3,8 +3,8 @@
 class PlanItem < ApplicationRecord
   belongs_to :plan
   belongs_to :user
-  has_one :schedule, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one :schedule, dependent: :destroy
 
   enum category: { move: 1, activity: 2, meal: 3, lodging: 4 }
 
@@ -12,6 +12,9 @@ class PlanItem < ApplicationRecord
   validates :category, presence: true
   validates :content, presence: true
   validates :budget, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   scope :order_by_updated_before, -> { order(updated_at: :desc) }
   scope :where_not_include_schedule, ->(schedule) { where.not(id: schedule.to_a.map { |item| item[:plan_item_id] }) }
