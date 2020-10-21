b7a539e6f8ab45e430c13b85906f925093b4a04b
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-10-21 14:04:01 +0900

    Add capistranoのデプロイ設定にSSHの設定を追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,9 @@
 
 server 'ourtrip.work', user: 'app', roles: %w[app db web]
 set :ssh_options, keys: '/Users/matsumoto/.ssh/id_rsa'
+
+set :ssh_options, {
+  port: 22,
+  forward_agent: true,
+  keys: ['~/.ssh/id_rsa']
+}
