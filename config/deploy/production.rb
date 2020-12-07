4bcca6ef1bd6d314816b17670e22c8f36e526164
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-12-07 16:12:12 +0900

    Add cicleCIにCDを追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,7 @@ server '3.113.117.184', user: 'app', roles: %w[app db web]
 
 set :ssh_options, {
   port: 22,
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
-  keys: ['~/.ssh/id_rsa']
+  auth_methods: %w[publickey]
 }
